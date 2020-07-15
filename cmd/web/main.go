072447f55667a28db9c880a2a1ef4456588a3dd5
Sanjeev <sanjeevraichur29@gmail.com>
2020-07-16 01:29:34 +0530

    Add: Home handler
    - Start web server at 8000
    - Add home handler at '/' route

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+
+	log.Println("Starting server on :8000")
+	err := http.ListenAndServe(":8000", mux)
+	log.Fatal(err)
+
+}

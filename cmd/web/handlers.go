dc6765f6601a10c7af574c8c3c5fc5ba5e4c57a2
Sanjeev <sanjeevraichur29@gmail.com>
2020-07-16 02:02:36 +0530

    Add: Template to home page
    - Add html template to home page
    - Parse template in home hander

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,7 +14,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello from Snippetbox!"))
+	// Parse home template file
+	ts, err := template.ParseFiles("../../ui/html/home.html")
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {

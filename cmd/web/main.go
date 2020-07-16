0389d81c4356204fbb9a18c7700fb6a7c7a68719
Sanjeev <sanjeevraichur29@gmail.com>
2020-07-16 21:33:53 +0530

    Add: Server static files
    - Add static file server as a handler at '/static/' route
    - Use static files in base.html template file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,12 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 	mux.HandleFunc("/snippet", showSnippet)
 
+	// File server to serve static files
+	fileServer := http.FileServer(http.Dir("../../ui/static/"))
+
+	// Register filesServer as handler
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :8000")
 	err := http.ListenAndServe(":8000", mux)
 	log.Fatal(err)

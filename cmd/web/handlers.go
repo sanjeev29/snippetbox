eed995f0c8690b5f042ca2e384f0e1a68ce7aba7
Sanjeev <sanjeevraichur29@gmail.com>
2020-07-24 01:04:37 +0530

    Add: Third party router
    - Update routes and handlers
    - Add new handler with dummy return to create snippet
    - Update dependency files
    - Update home template with new URL

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,11 +9,6 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -25,12 +20,6 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-
 	title := "Test title"
 	content := "This is the content for the test title"
 	expires := "7"
@@ -42,11 +31,11 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Redirect user to relevant page for the snippet
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet?%d", id), http.StatusSeeOther)
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -64,4 +53,8 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 	app.render(w, r, "show.page.tmpl", &templateData{Snippet: s})
 
+}
+
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
 }
